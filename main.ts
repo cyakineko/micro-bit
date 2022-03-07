27abf67537445a810d0cde4d37de282f061bd3d7
tatsuyacyato <98063568+cyakineko@users.noreply.github.com>
2022-03-07 12:13:10 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,59 @@
+input.onButtonPressed(Button.A, function () {
+    me.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    me.change(LedSpriteProperty.X, 1)
+})
+let mob4: game.LedSprite = null
+let mob3: game.LedSprite = null
+let mob2: game.LedSprite = null
 let mob: game.LedSprite = null
-let _1y = 0
-let y = 0
-let x = 0
-let bool = 0
-let _1x = 2
-led.plot(_1x, 4)
-while (!(x == _1x && y == 4)) {
-    _1y = _1x
-    if (input.buttonIsPressed(Button.A) && !(_1x == 0) && bool == 0) {
-        _1x += -1
-        bool = 1
-    }
-    if (input.buttonIsPressed(Button.B) && !(_1x == 4) && bool == 0) {
-        _1x += 1
-        bool = 1
-    }
-    led.unplot(_1y, 4)
-    led.plot(_1x, 4)
-    if (!(input.buttonIsPressed(Button.A) && input.buttonIsPressed(Button.B))) {
-        bool = 0
-    }
-}
-basic.showString("Game over")
+let nom = 0
+let 配列: number[] = []
+let me: game.LedSprite = null
+let a = 400
+me = game.createSprite(2, 4)
+game.setScore(0)
 basic.forever(function () {
-    mob = game.createSprite(randint(0, 4), 0)
-    while (!(mob.get(LedSpriteProperty.Y) == 4)) {
+    配列 = [
+    0,
+    1,
+    2,
+    3,
+    4
+    ]
+    nom = randint(1, 配列.length)
+    mob = game.createSprite(配列[nom], 0)
+    配列.removeAt(nom)
+    nom = randint(1, 配列.length)
+    mob2 = game.createSprite(配列[nom], 0)
+    配列.removeAt(nom)
+    nom = randint(1, 配列.length)
+    mob3 = game.createSprite(配列[nom], 0)
+    配列.removeAt(nom)
+    nom = randint(1, 配列.length)
+    mob4 = game.createSprite(配列[nom], 0)
+    配列.removeAt(nom)
+    while (mob.get(LedSpriteProperty.Y) != 4) {
+        basic.pause(a)
         mob.change(LedSpriteProperty.Y, 1)
+        mob3.change(LedSpriteProperty.Y, 1)
+        mob2.change(LedSpriteProperty.Y, 1)
+        mob4.change(LedSpriteProperty.Y, 1)
+    }
+    basic.pause(a)
+    mob.change(LedSpriteProperty.Y, 1)
+    mob3.change(LedSpriteProperty.Y, 1)
+    mob2.change(LedSpriteProperty.Y, 1)
+    mob4.change(LedSpriteProperty.Y, 1)
+    if (mob.isTouching(me) || mob4.isTouching(me) || (mob2.isTouching(me) || mob3.isTouching(me))) {
+        game.gameOver()
+        basic.showNumber(game.score())
     }
     mob.delete()
+    mob2.delete()
+    mob3.delete()
+    mob4.delete()
+    game.addScore(1)
+    a = 500 - Math.floor(game.score() / 10) * 100
 })
